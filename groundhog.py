2db77603003fc7572191b4ca5150eb51c103cb34
Thomaltarix <thomas.boue@epitech.eu>
2024-04-10 19:35:19 +0200

    :sparkles: Add the Groundhog class

--- groundhog.py
+++ groundhog.py
@@ -2,6 +2,55 @@
 
 import sys
 
+class Groundhog:
+    """
+    Class used to compute various statistics on a list of temperatures.
+    """
+
+    def __init__(self, period = 0):
+        """
+        Constructor of the class.
+        """
+        self.temperatures = []
+        self.period = period
+
+    def getUserInput(self):
+        """
+        Get the user input.
+        """
+        temperature = input()
+        if temperature == "STOP":
+            return False
+        try:
+            self.temperatures.append(float(temperature))
+            return True
+        except ValueError:
+            raise self.Error("Invalid input: please enter a number or STOP.")
+
+    def computeAverage(self):
+        """
+        Computes the temperature increase average observed on the last period.
+        """
+        return 0
+
+    def computeEvolution(self):
+        """
+        Computes the relative temperature evolution between the last given temperature and the temperature observed n-days ago.
+        """
+        return 0
+
+    def computeDeviation(self):
+        """
+        Computes the standard deviation of the temperatures observed during the last period.
+        """
+        return 0
+
+    def displayTrend(self):
+        """
+        Displays as soon as it detects a switch in global tendency or nothing if not.
+        """
+        return 0
+
 def main():
     return 0
 
