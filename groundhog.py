d299548daa84bce8b31ea73b992314284effb9c2
Thomaltarix <thomas.boue@epitech.eu>
2024-04-10 19:35:29 +0200

    :sparkles: Add an Error class to the main one

--- groundhog.py
+++ groundhog.py
@@ -14,6 +14,17 @@ class Groundhog:
         self.temperatures = []
         self.period = period
 
+    class Error(Exception):
+        """
+        Base class for exceptions in this module.
+        """
+
+        def __init__(self, message):
+            self.message = message
+
+        def __str__(self):
+            return self.message
+
     def getUserInput(self):
         """
         Get the user input.
